2cc3767f2ce9014846b5c35ee9dd5b1d0876233c
Maneshkhatri <2023n14128@gmail.com>
2026-09-05 01:47:10 -0700

    Upload PF Lab tasks

---
+++ task3_escape.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("-------------------------\n");
+    printf("\tID CARD\n");
+    printf("-------------------------\n");
+    printf("Name:\tManesh Kumar\n");
+    printf("Roll No:\t26K-1045\n");
+    printf("Department:\tComputer Science\n");
+    printf("University:\tFAST-NUCES\n");
+    printf("Quote: \"Hello\"\n");
+    printf("File: C:\\Student\n");
+    printf("-------------------------\n");
+
+    return 0;
+}
